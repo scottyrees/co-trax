bbcffec3126ccacdbd073c6e4f02eff69181df61
scott <scottrees83@hotmail.com>
2017-11-19 12:24:29 -0800

    coTrax app, first commit

--- cotrax/wsgi.py
+++ cotrax/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for protrax project.
+WSGI config for cotrax project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "protrax.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cotrax.settings")
 
 application = get_wsgi_application()
